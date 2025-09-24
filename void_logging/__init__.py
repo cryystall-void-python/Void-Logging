f783ede793ee62779dc2c1e18c298dd120a261c0
Mathieu Suchet <msuchet63@gmail.com>
2025-09-24 13:49:19 +0200

    better pyproject toml and adding api folder back

--- void_logging/__init__.py
+++ void_logging/__init__.py
@@ -1,5 +1,5 @@
-from .api import *
+from . import api
 from . import rlgym_learn
 from . import rocket_league
 
-__all__ = [*api.__all__, "rlgym_learn", "rocket_league"]
+__all__ = ["api", "rlgym_learn", "rocket_league"]
