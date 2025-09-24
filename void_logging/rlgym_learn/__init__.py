4e4ea4f5b263b7ba51573d98d56f60413edaa2b7
Mathieu Suchet <msuchet63@gmail.com>
2025-09-24 15:25:42 +0200

    fix wrong API

--- void_logging/rlgym_learn/__init__.py
+++ void_logging/rlgym_learn/__init__.py
@@ -1,5 +1,5 @@
 from .reward_logger import RewardLogger
 from .reward_shared_info_provider import RewardSharedInfoProvider
-from .metric_logger import CustomMetricLogger, CustomMetricSerde
+from .metric_logger import CustomMetricLogger, custom_metrics_serde
 
-__all__ = ["RewardLogger", "RewardSharedInfoProvider", "CustomMetricLogger", "CustomMetricSerde"]
+__all__ = ["RewardLogger", "RewardSharedInfoProvider", "CustomMetricLogger", "custom_metrics_serde"]
